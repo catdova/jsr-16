0dac9779bd02629867b174df22782f00fd0a08e5
Kim Cordova <cordova.k529@gmail.com>
2017-11-08 21:22:39 -0500

    save

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Initialize Firebase
-var config = {
-  apiKey: '<your-api-key>',
-  authDomain: '<your-auth-domain>',
-  databaseURL: '<your-database-url>',
-  storageBucket: '<your-storage-bucket>'
-};
-firebase.initializeApp(config);
+  var config = {
+    apiKey: "AIzaSyDPXp4P7U545xVtdI1NM5NHhwHETOFaxhM",
+    authDomain: "jsr-practice-class-16.firebaseapp.com",
+    databaseURL: "https://jsr-practice-class-16.firebaseio.com",
+    projectId: "jsr-practice-class-16",
+    storageBucket: "jsr-practice-class-16.appspot.com",
+    messagingSenderId: "597196518404"
+  };
+  firebase.initializeApp(config);
 
 $(document).ready(function() {
   var database = firebase.database();
@@ -13,7 +15,7 @@ $(document).ready(function() {
 
   // CREATE
 
-  $('#message-form').submit(function(event) {
+$('#message-form').submit(function(event) {
     // by default a form submit reloads the DOM which will subsequently reload all our JS
     // to avoid this we preventDefault()
     event.preventDefault();
@@ -33,12 +35,28 @@ $(document).ready(function() {
       votes: 0
     });
   });
-
+getFanMessages();
   // READ
   function getFanMessages() {
 
     // use reference to app database to listen for changes in messages data
     // hint: use something referring to 'value'
+    var messagesReference = database.ref('messages');
+
+    messagesReference.on('value', 
+      function(results){
+
+        var allMessages = results.val();
+        var $messageBoard = $('.message-board');
+
+        for (var comment in allMessages) {
+          var $li = $('<li></li>');
+          $li.html(allMessages[comment].message);
+
+          $messageBoard.append($li);
+        }
+
+      });
 
       // iterate through results coming from database call; messages
 
